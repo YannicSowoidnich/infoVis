77f24c5591c6ea012d8713eccecf76e4e1350ea7
yannic <yannic.sowoidnich@gmx.de>
2017-11-26 16:20:29 +0100

    fiddled around, tried to render the words

--- InfoVis_VA_Framework/src/de/uni_stuttgart/vis/submissions/Submission1_LastNameOfTheGroupLeader.java
+++ InfoVis_VA_Framework/src/de/uni_stuttgart/vis/submissions/Submission1_LastNameOfTheGroupLeader.java
@@ -1,33 +1,49 @@
 package de.uni_stuttgart.vis.submissions;
 
+import java.awt.Font;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import de.uni_stuttgart.vis.data.WeightedString;
 import de.uni_stuttgart.vis.data.wordcloud.DataProvider;
 import de.uni_stuttgart.vis.framework.InfoVisFramework;
 import de.uni_stuttgart.vis.geom.AbstractGeometry;
+import de.uni_stuttgart.vis.geom.Rectangle;
 
 public class Submission1_LastNameOfTheGroupLeader extends InfoVisFramework{
-	List<WeightedString> input = new DataProvider().getWords();
+	List<WeightedString> input;
+	List<AbstractGeometry> output;
+	AbstractGeometry entry;
+	Font defaultFont = new Rectangle(0,0,0,0).getFont();
+	Font font;
 	
 	public Submission1_LastNameOfTheGroupLeader() {
-		Collections.sort(input);
-		for (int i = 0; i<input.size(); i++) {
-			System.out.println("Word: " + input.get(i).getText() + "Frequenz: " + input.get(i).getFrequency() );
-		}
 		this.mapData();
 	}
-
-
-	@Override
-	public List<AbstractGeometry> mapData() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Rectangle createGeometry(String text, Font font) {
+		Rectangle rect = new Rectangle(20, 20, 20, 20);
+		rect.setText(text);
+		rect.setFont(font);
+		return rect;
 	}
+
 	
 	public static void main(String[] args) {
 		Submission1_LastNameOfTheGroupLeader sub1 = new Submission1_LastNameOfTheGroupLeader();
+	}
+
+	@Override
+	public List<AbstractGeometry> mapData() {
+		this.input = new DataProvider().getWords();
+		output = new LinkedList<AbstractGeometry>();
+		Collections.sort(this.input);
+		for (int i = 0; i<this.input.size(); i++) {		
+			font = new Font("Arial", defaultFont.getStyle(), input.get(i).getFrequency());
+			output.add(this.createGeometry(input.get(i).getText(), font));
+		}
+		return output;
 	}
 
 
